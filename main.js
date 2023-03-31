e7a0012d29a317e03eeb6e50aec16378d0b0ffae
burakakkanat <burakakkanat@windowslive.com>
2023-03-31 17:24:39 +0300

    New way of getting cpuTemp since the previous one was not working.

--- main.js
+++ main.js
@@ -1,8 +1,54 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const si = require('systeminformation')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const si = require('systeminformation');
+const edge = require('electron-edge-js');
+const path = require('path');
 
-let mainWindow
+const getOhmCpuTemp = edge.func({
+    source: function() {/*
+
+    using OpenHardwareMonitor.Hardware;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Linq;
+
+    public class Startup {
+
+        public async Task<object> Invoke(dynamic input) {
+
+            var cpuTemp = 0f;
+
+            Computer computer = new Computer() {
+                CPUEnabled = true,
+            };
+
+            computer.Open();
+
+            foreach (var hardware in computer.Hardware) {
+
+                if (hardware.HardwareType != HardwareType.CPU) {
+                    continue;
+                }
+
+                hardware.Update();
+
+                foreach (var sensor in hardware.Sensors) {
+                    if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("CPU Package")) {
+                        cpuTemp = sensor.Value.GetValueOrDefault();
+                    }
+                }
+            }
+
+            computer.Close();
+
+            return cpuTemp;
+        }
+    }
+    */
+    },
+    references: ['OpenHardwareMonitorLib.dll']
+})
+
+let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
@@ -12,21 +58,23 @@ function createWindow() {
         }
     })
 
-    mainWindow.setTitle('System Information')
+    mainWindow.setTitle('System Information');
 
-    mainWindow.loadFile('index.html')
+    mainWindow.loadFile('index.html');
 
     const sendSystemInfo = async () => {
         try {
 
-            const cpuTemp = await si.cpuTemperature();
+            const cpuTemp = await getOhmCpuTemp();
+
+            console.log(cpuTemp);
             const gpuData = await si.graphics();
 
             const cpuLoad = await si.currentLoad();
             const gpuLoad = (gpuData.controllers[0].memoryUsed * 100 / gpuData.controllers[0].memoryTotal).toFixed(1);
 
             const systemInfo = {
-                cpuTemp: cpuTemp.main.toFixed(1),
+                //cpuTemp: cpuTemp.main.toFixed(1),
                 gpuTemp: gpuData.controllers[0].temperatureGpu.toFixed(1),
                 cpuLoad: cpuLoad.currentLoad.toFixed(1),
                 gpuLoad: gpuLoad,
@@ -38,7 +86,7 @@ function createWindow() {
         }
     }
 
-    setInterval(sendSystemInfo, 1000);
+    setInterval(sendSystemInfo, 2500);
 }
 
 app.whenReady().then(() => {
